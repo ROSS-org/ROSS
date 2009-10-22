863663af6f9e3d79717a6bebb7228db0e86b2671
laprej <laprej>
2009-10-22 00:38:46 +0000

    Forgot to add this.

---
+++ models/rossnet/rossnet/config.h.cmake
@@ -0,0 +1,24 @@
+#ifndef _TEST_CONFIG_H
+#define _TEST_CONFIG_H
+
+#cmakedefine HAVE_IP_H 1
+
+#cmakedefine HAVE_TCP_H 1
+
+#cmakedefine HAVE_EPI_H 1
+
+#cmakedefine HAVE_OSPF_H 1
+
+#cmakedefine HAVE_BGP_H 1
+
+#cmakedefine HAVE_FTP_H 1
+
+#cmakedefine HAVE_PHOLD_H 1
+
+#cmakedefine HAVE_MULTICAST_H 1
+
+#cmakedefine HAVE_TLM_H 1
+
+#cmakedefine HAVE_RANDOM_WALK_H 1
+
+#endif _TEST_CONFIG_H
