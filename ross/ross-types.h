5ea295d173409691659861ffcc32cf1901ef077c
anderm7@LAB.CS.RPI.EDU <anderm7@LAB.CS.RPI.EDU>
2010-06-24 23:27:03 +0000

    Consolidated, Updated, and Removed Copyright Stuff

--- ross/ross-types.h
+++ ross/ross-types.h
@@ -5,54 +5,6 @@
  *  @brief Definition of ROSS basic types
  */
 
-/*
- * ROSS: Rensselaer's Optimistic Simulation System.
- * Copyright (c) 1999-2003 Rensselaer Polytechnic Instutitute.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *
- *      This product includes software developed by David Bauer,
- *      Dr. Christopher D.  Carothers, and Shawn Pearce of the
- *      Department of Computer Science at Rensselaer Polytechnic
- *      Institute.
- *
- * 4. Neither the name of the University nor of the developers may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * 5. The use or inclusion of this software or its documentation in
- *    any commercial product or distribution of this software to any
- *    other party without specific, written prior permission is
- *    prohibited.
- *
- * THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY AND CONTRIBUTORS ``AS
- * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
- * REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *
- */
-
 typedef struct tw_statistics tw_statistics;
 typedef struct tw_pq tw_pq;
 typedef struct tw_lptype tw_lptype;
