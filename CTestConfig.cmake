9a09a8bc11d1697e7bf2899d2f55e751327a7914
laprej@LAB.CS.RPI.EDU <laprej@LAB.CS.RPI.EDU>
2010-07-13 17:06:16 +0000

    Changed CDash to point to odin.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -8,6 +8,6 @@ set(CTEST_PROJECT_NAME "ROSS")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=ROSS")
+set(CTEST_DROP_SITE "odin.cs.rpi.edu")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=ROSS")
 set(CTEST_DROP_SITE_CDASH TRUE)
