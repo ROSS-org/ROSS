67de31d2627fc5cfb1015f1f65a782c1f93c0fd0
laprej@LAB.CS.RPI.EDU <laprej@LAB.CS.RPI.EDU>
2013-01-22 20:03:06 +0000

    Updated avlInsert to care about send_pe.

--- ross/avl_tree.c
+++ ross/avl_tree.c
@@ -167,42 +167,41 @@ avlInsert(AvlTree *t, tw_event *key)
         /* done */
         return;
     }
-    else if (key->recv_ts == (*t)->key->recv_ts) {
-        /* nothing to do */
-        //printf("Trying to insert duplicate events\n");
-        //printf("state.owner is [old : new] [%d : %d]\n",
-        //       (*t)->key->state.owner, key->state.owner);
-        // SOMETHING TO DO IS CHECK THE QUEUE AND EVENT ID
-        if ((*t)->key->state.owner == TW_pe_free_q) {
-            //printf("Replacing old event in AVL tree\n");
-            (*t)->key = key;
-            return;
-        }
-        
+    
+    if (key->recv_ts == (*t)->key->recv_ts) {
+        // We have a timestamp tie, check the event ID
         if (key->event_id == (*t)->key->event_id) {
-            //printf("Event IDs are the same!\n");
-        }
-        else {
-            //printf("Event IDs are different! Saving the new one as well.\n");
-            //(*t)->key = key;
-            if (key->event_id > (*t)->key->event_id) {
-                avlInsert(&(*t)->child[1], key);
+            // We have a event ID tie, check the send_pe
+            if (key->send_pe == (*t)->key->send_pe) {
+                // This cannot happen
+                assert(0 && "The events are identical!!!\n");
             }
             else {
-                avlInsert(&(*t)->child[0], key);
+                avlInsert(&(*t)->child[key->send_pe > (*t)->key->send_pe], key);
+                avlRebalance(t);
             }
+        }
+        else {
+            avlInsert(&(*t)->child[key->event_id > (*t)->key->event_id], key);
+//            if (key->event_id > (*t)->key->event_id) {
+//                avlInsert(&(*t)->child[1], key);
+//            }
+//            else {
+//                avlInsert(&(*t)->child[0], key);
+//            }
             avlRebalance(t);
         }
         return;
     }
     else {
         /* do the insert in subtree */
-        if (key->recv_ts > (*t)->key->recv_ts) {
-            avlInsert(&(*t)->child[1], key);
-        }
-        else {
-            avlInsert(&(*t)->child[0], key);
-        }
+        avlInsert(&(*t)->child[key->recv_ts > (*t)->key->recv_ts], key);
+//        if (key->recv_ts > (*t)->key->recv_ts) {
+//            avlInsert(&(*t)->child[1], key);
+//        }
+//        else {
+//            avlInsert(&(*t)->child[0], key);
+//        }
         //        avlInsert(&(*t)->child[key > (*t)->key], key);
         
         avlRebalance(t);
