c181cae5f1fd86cbaa9552c4e134024fc792afd5
Elsa Gonsiorowski (Uranus) <me@gonsie.com>
2014-02-21 16:38:07 -0500

    removed state_qh from tw_lp

--- ross/ross-types.h
+++ ross/ross-types.h
@@ -366,7 +366,6 @@ struct tw_lp
   tw_kp         *kp; /**< @brief kp -- Kernel process that we belong to (must match pe). */
 
   void		*cur_state; /**< @brief Current application LP data */
-  tw_lp_state	*state_qh; /**< @brief Head of [free] state queue (for state saving) */
   tw_lptype	 type; /**< @brief Type of this LP, including service callbacks */
   tw_rng_stream	*rng; /**< @brief  RNG stream array for this LP */
 };
