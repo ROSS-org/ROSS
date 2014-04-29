37a1e51d687956b1b0c779d09677f5573bf54894
Justin M. LaPre <justin@lapre.com>
2014-04-29 16:10:14 -0400

    Eye on the prize!
    This is ultimately what we would like to work. It doesn't
    seem to at the moment but stay tuned.
    
    Removed the hash from conservative mode.

--- ross/network-mpi.c
+++ ross/network-mpi.c
@@ -107,7 +107,14 @@ tw_net_start(void)
 
   tw_pe_create(1);
   tw_pe_init(0, g_tw_mynode);
-  g_tw_pe[0]->hash_t = tw_hash_create();
+
+  /* If we're in (some variation of) optimistic mode, we need this hash */
+  if (g_tw_synchronization_protocol == OPTIMISTIC ||
+      g_tw_synchronization_protocol == OPTIMISTIC_DEBUG) {
+    g_tw_pe[0]->hash_t = tw_hash_create();
+  } else {
+    g_tw_pe[0]->hash_t = NULL;
+  }
 
   if (send_buffer < 1)
     tw_error(TW_LOC, "network send buffer must be >= 1");
@@ -365,8 +372,11 @@ recv_finish(tw_pe *me, tw_event *e, char * buffer)
       return;
     }
 
-  tw_hash_insert(me->hash_t, e, e->send_pe);
-  e->state.remote = 1;
+  if (g_tw_synchronization_protocol == OPTIMISTIC ||
+      g_tw_synchronization_protocol == OPTIMISTIC_DEBUG) {
+    tw_hash_insert(me->hash_t, e, e->send_pe);
+    e->state.remote = 1;
+  }
 
 #if ROSS_MEMORY
   mem_size = (size_t) e->memory;
