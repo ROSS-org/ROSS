169b153bf2473dc1c671e184f811ec15f1eb1942
Justin M. LaPre <justin@lapre.com>
2014-12-15 16:22:07 -0500

    jci.

--- ross/buddy.c
+++ ross/buddy.c
@@ -1,4 +1,32 @@
 #include "buddy.h"
+#include <stdlib.h>
+
+#define BUDDY_BLOCK_ORDER 5 /**< @brief Minimum block order */
+
+/**
+ * Pass in the power of two e.g., passing 5 will yield 2^5 = 32
+ * The smallest order we'll create will be 32 so this would yield one list
+ */
+buddy_list_t * create_buddy_table(unsigned int power_of_two)
+{
+    int i;
+    int list_count;
+    buddy_list_t *bsystem;
+    
+    if (power_of_two < BUDDY_BLOCK_ORDER) {
+        power_of_two = BUDDY_BLOCK_ORDER;
+    }
+    
+    list_count = power_of_two - 5 + 1;
+    
+    bsystem = calloc(list_count, sizeof(buddy_list_t));
+    
+    for (i = 0; i < list_count; i++) {
+        bsystem[i].order = i + BUDDY_BLOCK_ORDER;
+    }
+    
+    return bsystem;
+}
 
 /**
  * From http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
@@ -7,9 +35,9 @@
 unsigned int
 next_power2(unsigned int v)
 {
-    // We're not allocating chunks smaller than 32 bytes
-    if (v < 32) {
-        return 32;
+    // We're not allocating chunks smaller than BUDDY_MIN_SZ bytes
+    if (v < (2 << BUDDY_BLOCK_ORDER)) {
+        return (2 << BUDDY_BLOCK_ORDER);
     }
 
     v--;
--- ross/buddy.h
+++ ross/buddy.h
@@ -1,6 +1,14 @@
 #ifndef BUDDY_H
 #define BUDDY_H
 
+typedef struct buddy_list
+{
+    void *ptr;
+    struct buddy_list *next;
+    unsigned int order;
+} buddy_list_t;
+
+buddy_list_t * create_buddy_table(unsigned int power_of_two);
 unsigned int next_power2(unsigned int v);
 
 #endif /* BUDDY_H */
