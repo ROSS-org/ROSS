6006e878b7c4b6cfdd1aa36f79998d38f82fb0c9
Justin M. LaPre <justin@lapre.com>
2015-01-09 13:09:18 -0500

    One more spot to grab the pages back early.

--- ross/tw-state.c
+++ ross/tw-state.c
@@ -90,6 +90,11 @@ tw_state_rollback(tw_lp *lp, tw_event *revent)
 		&revent->cv,
 		tw_event_data(revent),
 		lp);
+
+        if (revent->delta_buddy) {
+            buddy_free(revent->delta_buddy, g_tw_buddy_master);
+            revent->delta_buddy = 0;
+        }
 #if 0
     } else
       {
