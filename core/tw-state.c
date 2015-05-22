2e447188d7e7697ed9fe29fa57b7576829f30d13
Justin M. LaPre <justin@lapre.com>
2015-05-22 14:04:51 -0400

    Cleaning up.

--- core/tw-state.c
+++ core/tw-state.c
@@ -52,10 +52,19 @@ tw_snapshot_delta(tw_lp *lp, size_t state_sz)
 void
 tw_snapshot_restore(tw_lp *lp, size_t state_sz, void *buffer, size_t delta_size)
 {
+    int i;
     tw_clock start = tw_clock_read();
-    int ret = LZ4_decompress_safe(buffer, lp->cur_state, delta_size, state_sz);
+    unsigned char *snapshot = buffer;
+    unsigned char *current_state = lp->cur_state;
+
+    int ret = LZ4_decompress_safe(buffer, (char*)lp->pe->delta_buffer[0], delta_size, state_sz);
     g_tw_pe[0]->stats.s_lz4 += (tw_clock_read() - start);
     if (ret < 0) {
         tw_error(TW_LOC, "LZ4_decompress_fast error");
+    }
+
+    snapshot = lp->pe->delta_buffer[0];
+    for (i = 0; i < state_sz; i++) {
+        current_state[i] = current_state[i] - snapshot[i];
     }
 }
