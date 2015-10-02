0f53b3b5dda2264d6465deeb86ad138b726d7d4c
Justin M. LaPre <justin@lapre.com>
2015-10-02 17:38:15 -0400

    Simplify delta API.

--- core/tw-state.c
+++ core/tw-state.c
@@ -55,14 +55,14 @@ tw_snapshot_delta(tw_lp *lp, size_t state_sz)
  * Restore the state of lp to the (decompressed) data held in buffer
  */
 void
-tw_snapshot_restore(tw_lp *lp, size_t state_sz, void *buffer, size_t delta_size)
+tw_snapshot_restore(tw_lp *lp, size_t state_sz)
 {
     int i;
     tw_clock start = tw_clock_read();
-    unsigned char *snapshot = (unsigned char *)buffer;
+    unsigned char *snapshot = (unsigned char *)lp->pe->cur_event->delta_buddy;
     unsigned char *current_state = (unsigned char *)lp->cur_state;
 
-    int ret = LZ4_decompress_fast((char *)buffer, (char*)lp->pe->delta_buffer[0], state_sz);
+    int ret = LZ4_decompress_fast((char *)snapshot, (char*)lp->pe->delta_buffer[0], state_sz);
     g_tw_pe[0]->stats.s_lz4 += (tw_clock_read() - start);
     if (ret < 0) {
         tw_error(TW_LOC, "LZ4_decompress_fast error");
