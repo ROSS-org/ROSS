2c9995d28d79bb6aaedddc3f164bfcc35656c8f8
Elsa Gonsiorowski (Eris) <gonsie@me.com>
2016-05-11 12:55:06 -0400

    commit function for phold

--- models/phold/phold.c
+++ models/phold/phold.c
@@ -88,6 +88,10 @@ phold_event_handler_rc(phold_state * s, tw_bf * bf, phold_message * m, tw_lp * l
 		tw_rand_reverse_unif(lp->rng);
 }
 
+void phold_commit(phold_state * s, tw_bf * bf, phold_message * m, tw_lp * lp)
+{
+}
+
 void
 phold_finish(phold_state * s, tw_lp * lp)
 {
@@ -99,6 +103,7 @@ tw_lptype       mylps[] = {
      (pre_run_f) NULL,
 	 (event_f) phold_event_handler,
 	 (revent_f) phold_event_handler_rc,
+	 (commit_f) phold_commit,
 	 (final_f) phold_finish,
 	 (map_f) phold_map,
 	sizeof(phold_state)},
