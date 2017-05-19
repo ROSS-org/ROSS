fc6aed0f8f96eec19f0641365e9732d109c3b0b6
Caitlin Ross <rossc3@rpi.edu>
2017-05-19 17:02:27 -0400

    adding Damaris plugins for creating visualizations in situ

--- core/damaris/damaris.c
+++ core/damaris/damaris.c
@@ -159,6 +159,9 @@ void st_expose_data_damaris(tw_pe *me, tw_stime gvt, tw_statistics *s, int inst_
         rt_block_counter++;
 
     memcpy(&last_stats, s, sizeof(tw_statistics));
+
+    if ((err = damaris_signal("test")) != DAMARIS_OK)
+        damaris_error(TW_LOC, err, "test");
 }
 
 /**
