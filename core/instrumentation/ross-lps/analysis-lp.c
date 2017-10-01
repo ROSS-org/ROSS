42cadf843c1a27581668d0a1fa0f4699071a7dcb
Caitlin Ross <rossc3@rpi.edu>
2017-09-30 20:46:43 -0400

    adding support for analysis LPs to save sim engine data

--- core/instrumentation/ross-lps/analysis-lp.c
+++ core/instrumentation/ross-lps/analysis-lp.c
@@ -60,6 +60,20 @@ void analysis_init(analysis_state *s, tw_lp *lp)
     s->start_sample = 0;
     s->current_sample = -1;
 
+    // set up sample structs for sim engine data
+    s->prev_data_kp.time_ahead_gvt = 0;
+    s->prev_data_kp.rb_total = 0;
+    s->prev_data_kp.rb_secondary = 0;
+
+    s->prev_data_lp = tw_calloc(TW_LOC, "analysis LPs", sizeof(sim_engine_data_lp), s->num_lps);
+    for (i = 0; i < s->num_lps; i++)
+    {
+        s->prev_data_lp[i].nevent_processed = 0; 
+        s->prev_data_lp[i].e_rbs = 0;
+        s->prev_data_lp[i].nsend_network = 0;
+        s->prev_data_lp[i].nread_network = 0;
+    }
+
     // schedule 1st sampling event 
     st_create_sample_event(lp);
 }
@@ -86,6 +100,9 @@ void analysis_event(analysis_state *s, tw_bf *bf, analysis_msg *m, tw_lp *lp)
 
         model_lp->model_types->sample_event_fn(model_lp->cur_state, bf, lp, sample->lp_data[i]);
     } 
+
+    // sim engine sampling
+    collect_sim_engine_data(lp->pe, lp, s, (tw_stime) tw_clock_read() / g_tw_clock_rate);
     
     // create next sampling event
     st_create_sample_event(lp);
@@ -140,8 +157,11 @@ void analysis_commit(analysis_state *s, tw_bf *bf, analysis_msg *m, tw_lp *lp)
             {
                 model_lp = tw_getlocal_lp(s->lp_list[j]);
                 metadata.lpid = model_lp->gid;
+                metadata.kpid = model_lp->kp->id;
+                metadata.peid = model_lp->pe->id;
                 metadata.ts = m->timestamp;
                 metadata.sample_sz = model_lp->model_types->sample_struct_sz;
+                metadata.flag = MODEL_TYPE;
 
                 char buffer[sizeof(lp_metadata) + model_lp->model_types->sample_struct_sz];
                 memcpy(&buffer[0], (char*)&metadata, sizeof(lp_metadata));
@@ -214,4 +234,62 @@ tw_lptype analysis_lp[] = {
 void st_analysis_lp_settype(tw_lpid lpid)
 {
     tw_lp_settype(lpid, &analysis_lp[0]);
+}
+
+// although collected at virtual time sampling intervals, do not roll back this collected data
+// i.e., immediately put into the data buffer instead of waiting until commit events
+// TODO should we collect perf data on all LPs on this KP, or just the ones we're collecting model data for?
+void collect_sim_engine_data(tw_pe *pe, tw_lp *lp, analysis_state *s, tw_stime current_rt)
+{
+    lp_metadata metadata;
+    tw_lp *cur_lp;
+    sim_engine_data_kp cur_data_kp;
+    sim_engine_data_lp cur_data_lp;
+    char kp_buffer[sizeof(lp_metadata) + sizeof(sim_engine_data_kp)];
+    char lp_buffer[sizeof(lp_metadata) + sizeof(sim_engine_data_lp)];
+    int i;
+
+    // kp data
+    metadata.lpid = lp->gid;
+    metadata.kpid = lp->kp->id;
+    metadata.peid = lp->pe->id;
+    metadata.ts = tw_now(lp);
+    metadata.real_time = current_rt;
+    metadata.sample_sz = sizeof(sim_engine_data_kp);
+    metadata.flag = KP_TYPE;
+
+    cur_data_kp.time_ahead_gvt = lp->kp->last_time - pe->GVT;
+    cur_data_kp.rb_total = lp->kp->s_rb_total - s->prev_data_kp.rb_total;
+    cur_data_kp.rb_secondary = lp->kp->s_rb_secondary - s->prev_data_kp.rb_secondary;
+    s->prev_data_kp.rb_total = lp->kp->s_rb_total;
+    s->prev_data_kp.rb_total = lp->kp->s_rb_secondary;
+
+    memcpy(&kp_buffer[0], (char*)&metadata, sizeof(lp_metadata));
+    memcpy(&kp_buffer[sizeof(lp_metadata)], (char*)&cur_data_kp, sizeof(sim_engine_data_kp));
+
+    st_buffer_push(ANALYSIS_LP, &kp_buffer[0], sizeof(lp_metadata) + sizeof(sim_engine_data_kp));
+
+    // lp data
+    metadata.sample_sz = sizeof(sim_engine_data_lp);
+    metadata.flag = LP_TYPE;
+    for (i = 0; i < s->num_lps; i++)
+    {
+        cur_lp = tw_getlocal_lp(s->lp_list[i]);
+
+        metadata.lpid = cur_lp->gid;
+
+        cur_data_lp.nevent_processed = cur_lp->event_counters->s_nevent_processed - s->prev_data_lp[i].nevent_processed;
+        cur_data_lp.e_rbs = cur_lp->event_counters->s_e_rbs - s->prev_data_lp[i].e_rbs;
+        cur_data_lp.nsend_network = cur_lp->event_counters->s_nsend_network - s->prev_data_lp[i].nsend_network;
+        cur_data_lp.nread_network = cur_lp->event_counters->s_nread_network - s->prev_data_lp[i].nread_network;
+        s->prev_data_lp[i].nevent_processed = cur_lp->event_counters->s_nevent_processed;
+        s->prev_data_lp[i].e_rbs = cur_lp->event_counters->s_e_rbs;
+        s->prev_data_lp[i].nsend_network = cur_lp->event_counters->s_nsend_network;
+        s->prev_data_lp[i].nread_network = cur_lp->event_counters->s_nread_network;
+
+        memcpy(&lp_buffer[0], (char*)&metadata, sizeof(lp_metadata));
+        memcpy(&lp_buffer[sizeof(lp_metadata)], (char*)&cur_data_lp, sizeof(sim_engine_data_lp));
+
+        st_buffer_push(ANALYSIS_LP, &lp_buffer[0], sizeof(lp_metadata) + sizeof(sim_engine_data_lp));
+    }
 }
--- core/instrumentation/ross-lps/analysis-lp.h
+++ core/instrumentation/ross-lps/analysis-lp.h
@@ -5,20 +5,17 @@
 
 typedef struct analysis_state analysis_state;
 typedef struct analysis_msg analysis_msg;
-//typedef struct sim_engine_data sim_engine_data;
+typedef struct sim_engine_data_kp sim_engine_data_kp;
+typedef struct sim_engine_data_lp sim_engine_data_lp;
 typedef struct model_sample_data model_sample_data;
 typedef struct lp_metadata lp_metadata;
 
-struct analysis_state
-{
-    tw_lpid analysis_id; // id among analysis LPs only
-    int num_lps;
-    tw_lpid *lp_list; // list of LPs that the analysis LP is responsible for
-    model_sample_data *model_samples;
-    int start_sample;
-    int current_sample;
-    unsigned long long sample_sz;
-};
+typedef enum{
+    PE_TYPE,
+    KP_TYPE,
+    LP_TYPE,
+    MODEL_TYPE
+} data_type_flag;
 
 struct analysis_msg
 {
@@ -30,14 +27,28 @@ struct analysis_msg
 struct lp_metadata
 {
     tw_lpid lpid;
+    tw_kpid kpid;
+    tw_peid peid;
     tw_stime ts;
-    int64_t sample_sz;
+    tw_stime real_time;
+    int sample_sz;
+    int flag; // 0 == PE, 1 == KP, 2 == LP, 3 == model
 };
 
-//struct sim_engine_data
-//{
-//
-//};
+struct sim_engine_data_kp
+{
+    tw_stime time_ahead_gvt;
+    tw_stat rb_total; 
+    tw_stat rb_secondary;
+};
+
+struct sim_engine_data_lp
+{
+    tw_stat nevent_processed;
+    tw_stat e_rbs;
+    tw_stat nsend_network;
+    tw_stat nread_network;
+};
 
 struct model_sample_data
 {
@@ -45,11 +56,25 @@ struct model_sample_data
     void **lp_data;          /* data for each LP on the associated KP at this sampling point */
 };
 
+struct analysis_state
+{
+    tw_lpid analysis_id; // id among analysis LPs only
+    int num_lps;
+    tw_lpid *lp_list; // list of LPs that the analysis LP is responsible for
+    model_sample_data *model_samples;
+    int start_sample;
+    int current_sample;
+    unsigned long long sample_sz;
+    sim_engine_data_kp prev_data_kp;
+    sim_engine_data_lp *prev_data_lp;
+};
+
 void analysis_init(analysis_state *s, tw_lp *lp);
 void analysis_event(analysis_state *s, tw_bf *bf, analysis_msg *m, tw_lp *lp);
 void analysis_event_rc(analysis_state *s, tw_bf *bf, analysis_msg *m, tw_lp *lp);
 void analysis_commit(analysis_state *s, tw_bf *bf, analysis_msg *m, tw_lp *lp);
 void analysis_finish(analysis_state *s, tw_lp *lp);
+void collect_sim_engine_data(tw_pe *pe, tw_lp *lp, analysis_state *s, tw_stime current_rt);
 tw_peid analysis_map(tw_lpid gid);
 
 extern tw_lpid analysis_start_gid;
