2b28e848dfeae7b76bd80e4ba3f9896fd5f4ad74
Caitlin Ross <rossc3@rpi.edu>
2017-11-18 23:22:52 -0500

    change in how to determine when to limit optimism

--- core/perf-opt/perf-limit-optimism.c
+++ core/perf-opt/perf-limit-optimism.c
@@ -3,6 +3,7 @@
 static int limit_opt = LIMIT_OFF;
 static pe_data last_check = {0};
 int g_perf_disable_opt = 0;
+static tw_stime max_diff_sim = 0.0;
 
 static const tw_optdef perf_options[] = {
     TWOPT_GROUP("ROSS Performance Optimizations"),
@@ -38,40 +39,55 @@ double perf_efficiency_check(tw_pe *pe)
     tw_kp *kp;
     tw_stat nevent_delta;
     tw_stat e_rbs_delta;
-    double efficiency = 0;
-    double lowest_eff = DBL_MAX;
+    //double efficiency = 0;
+    //double lowest_eff = DBL_MAX;
+    tw_stime vt_diff = 0.0;
+    tw_stime max_diff = 0.0;
 
     for (i = 0; i < g_tw_nkp; i++)
     {
         kp = tw_getkp(i);
-        nevent_delta = kp->s_nevent_processed - last_check.nevent_processed[i];
-        e_rbs_delta = kp->s_e_rbs - last_check.e_rbs[i];
-        if (nevent_delta - e_rbs_delta != 0)
-            efficiency = 100.0 * (1 - ((double)e_rbs_delta/(double)(nevent_delta - e_rbs_delta)));
-        else
-            efficiency = 0;
+        vt_diff = kp->last_time - pe->GVT;
 
-        if (efficiency < lowest_eff)
-            lowest_eff = efficiency;
+        if (vt_diff > max_diff)
+            max_diff = vt_diff;
 
-        last_check.nevent_processed[i] = kp->s_nevent_processed;
-        last_check.e_rbs[i] = kp->s_e_rbs;
+        //nevent_delta = kp->s_nevent_processed - last_check.nevent_processed[i];
+        //e_rbs_delta = kp->s_e_rbs - last_check.e_rbs[i];
+        //if (nevent_delta - e_rbs_delta != 0)
+        //    efficiency = 100.0 * (1 - ((double)e_rbs_delta/(double)(nevent_delta - e_rbs_delta)));
+        //else
+        //    efficiency = 0;
+
+        //if (efficiency < lowest_eff)
+        //    lowest_eff = efficiency;
+
+        //last_check.nevent_processed[i] = kp->s_nevent_processed;
+        //last_check.e_rbs[i] = kp->s_e_rbs;
 
     }
 
-    efficiency = lowest_eff;
+    vt_diff = max_diff;
+    tw_stime ratio = vt_diff/max_diff_sim;
+    //efficiency = lowest_eff;
 
     if (limit_opt == LIMIT_OFF || limit_opt == LIMIT_RECOVER)
     {
-        if (efficiency < 0.0) // set flag to limit optimism
+        //if (efficiency < 0.0) // set flag to limit optimism
+        if (vt_diff > max_diff_sim)
             limit_opt = LIMIT_ON;
     }
     else if (limit_opt == LIMIT_ON)
     {
-        if (efficiency > 0)
+        //if (efficiency > 0)
+        if (vt_diff <= max_diff_sim)
             limit_opt = LIMIT_RECOVER;
     }
-    return efficiency;
+    //return efficiency;
+    if (vt_diff > max_diff_sim)
+        max_diff_sim = vt_diff;
+
+    return ratio;
 }
 
 void perf_adjust_optimism(tw_pe *pe)
@@ -79,9 +95,12 @@ void perf_adjust_optimism(tw_pe *pe)
     if (g_perf_disable_opt)
         return;
 
-    double efficiency = perf_efficiency_check(pe);
-    int inc_amount = 1000 * efficiency;
-    double dec_amt = 1.0 - (abs(efficiency) / (abs(efficiency) + 1000.0));
+    //double efficiency = perf_efficiency_check(pe);
+    tw_stime diff_ratio = perf_efficiency_check(pe);
+    //int inc_amount = 1000 * efficiency;
+    int inc_amount = 1000 * (1-diff_ratio);
+    //double dec_amt = 1.0 - (abs(efficiency) / (abs(efficiency) + 1000.0));
+    double dec_amt = diff_ratio;
     if (dec_amt < 0.05)
         dec_amt = 0.05;
     else if (dec_amt > 0.95)
