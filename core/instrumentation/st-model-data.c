bc310d152d00bf7d780020e6a5ecf1f15769bcb9
Caitlin Ross <caitlinross25@gmail.com>
2018-06-19 14:01:04 -0500

    Analysis LPs for virtual time sampling (#127)
    Adding virtual time sampling feature to ROSS instrumentation along with a major clean up of all of the instrumentation code.
    
    * improvements to buffer
    
    * removing redundancies related to MPI_COMM_ROSS
    
    * addition of Analysis LPs
    
    * fixing bugs in analysis lps
    
    * fixing memory allocation for analysis lp's sampling data
    
    * fixed memcpy bugs and added in call to sample buffer finalize
    
    * adding metadata for reading samples
    
    * minor fix
    
    * fix involving sampling end time
    
    * adding support for analysis LPs to save sim engine data
    
    * collect sim perf data on all LPs, not just ones used for model collection
    
    * make analysis LPs work in sequential mode
    
    * fix bug in committing data
    
    * adding option to only collect model data with analysis LPs
    
    * fixed bug in st_buffer_write (incorrect offsets)
    
    * changed model sample storage from array to linked list
    
    * toggle granularity of sim engine data collected by analysis LPs
    
    * KP now collects summed LP data
    
    * fixed bug that prevented writing out remaining buffer data at end of simulation
    
    * analysis LPs collecting PE data
    
    * PE data should only be collected by one sampling LP on that PE
    
    * dynamically limit optimism based on efficiency
    
    * disable optimization option
    
    * separating out the analysis LP info in final ROSS output
    
    * moved optimization outside of instrumentation
    
    * fix bugs in limiting optimism
    
    * improvements to dynamically limiting optimism
    
    * minor fix in dynamic lookahead changes
    
    * changes to lookahead optimization
    
    * adding metric to instrumentation
    
    * more metrics to PE instrumentation
    
    * change in how to determine when to limit optimism
    
    * updates to limiting optimisim
    
    * changing dynamic optimism to decrease only
    
    * removing stuff related to dynamically limiting optimism
    
    * can set path for instrumentation output
    
    * fixing bug in virtual time sampling for models that don't need it
    
    * refactor of simulation engine instrumentation to make it much simpler
    
    * removing some unnecessary stuff from instrumentation
    
    * some minor reorg in instrumentation
    
    * cleaning up event tracing
    
    * cleaning up model RT and GVT sampling
    
    * cleaning up a lot of analysis lp code to reuse the regular instrumentation code
    
    * fixing issues brought up with TravisCI c++ build
    
    * updating TravisCI tests for instrumentation
    
    * bug fix and some more updates to Travis tests
    
    * bug fix in analysis LPs

--- core/instrumentation/st-model-data.c
+++ core/instrumentation/st-model-data.c
@@ -32,62 +32,58 @@ void st_model_settype(tw_lpid i, st_model_types *model_types)
     else if (!model_type_warned && g_tw_mynode == g_tw_masternode)
     {
         fprintf(stderr, "WARNING: node: %ld: %s:%i: ", g_tw_mynode, __FILE__, __LINE__);
-        fprintf(stderr, "The struct st_model_types has not been defined! No model level data will be collected\n");
+        fprintf(stderr, "The struct st_model_types has not been defined for at least 1 LP type! No model level data will be collected for LP types without a valid st_model_types struct defined.\n");
         model_type_warned = 1;
     }
 }
 
 /*
- * This function allows for ROSS to collect model level data.
+ * This function allows for ROSS to collect model level data, when not using Analysis LPs.
  * Call this function when collecting simulation level data (GVT-based and/or real time-based).
  * Loop through all LPs on this PE and collect stats
  */
 void st_collect_model_data(tw_pe *pe, tw_stime current_rt, int stats_type)
 {
-    int index, lpid = 0;
-    int initial_sz = sizeof(tw_lpid) + sizeof(tw_stime) * 2 + sizeof(int);
-    int model_sz, total_sz = 0;
-    char *model_buffer = NULL;
-    tw_lp *clp;
     tw_clock start_cycle_time = tw_clock_read();
+    int index, lpid = 0;
+    int total_sz = 0;
+    tw_lp *clp;
+    sample_metadata sample_md;
+    model_metadata model_md;
+    sample_md.flag = MODEL_TYPE;
+    sample_md.sample_sz = sizeof(model_md);
+    sample_md.real_time = current_rt;
+    model_md.peid = (unsigned int) g_tw_mynode;
+    model_md.gvt = (float) pe->GVT;
+    model_md.stats_type = stats_type;
 
     for (lpid = 0; lpid < g_tw_nlp; lpid++) 
     {
         index = 0;
         clp = g_tw_lp[lpid];
-        if (!clp->model_types)
+        if (!clp->model_types || !clp->model_types->model_stat_fn)
         {
             // may not want to collect model stats on every LP type, so if not defined, just continue
             continue;
         }
 
-        if (!clp->model_types->model_stat_fn)
-        {
-            // may not want to collect model stats on every LP type, so if not defined, just continue
-            continue;
-        }
-
-        model_sz = clp->model_types->mstat_sz;
-        total_sz = initial_sz + model_sz;
+        sample_md.ts = tw_now(clp);
+        model_md.kpid = (unsigned int) clp->kp->id;
+        model_md.lpid = (unsigned int) clp->gid;
+        model_md.model_sz = (unsigned int) clp->model_types->mstat_sz;
+        total_sz = sizeof(sample_md) + sizeof(model_md) + model_md.model_sz;
         char buffer[total_sz];
+        memcpy(&buffer[0], &sample_md, sizeof(sample_md));
+        index += sizeof(sample_md);
+        memcpy(&buffer[index], &model_md, sizeof(model_md));
+        index += sizeof(model_md);
 
-        if (model_sz > 0)
+        if (model_md.model_sz > 0)
         {
-            // TODO want to have KP LVT instead of GVT?
-            memcpy(&buffer[index], &(clp->gid), sizeof(tw_lpid));
-            index += sizeof(tw_lpid);
-            memcpy(&buffer[index], &current_rt, sizeof(tw_stime));
-            index += sizeof(tw_stime);
-            memcpy(&buffer[index], &pe->GVT, sizeof(tw_stime));
-            index += sizeof(tw_stime);
-            memcpy(&buffer[index], &stats_type, sizeof(int));
-            index += sizeof(int);
-            if (index != initial_sz)
-                printf("WARNING: size of data being pushed to buffer is incorrect!\n");
             (*clp->model_types->model_stat_fn)(clp->cur_state, clp, &buffer[index]);
 
             if (g_tw_synchronization_protocol != SEQUENTIAL)
-                st_buffer_push(g_st_buffer[MODEL_COL], &buffer[0], total_sz);
+                st_buffer_push(MODEL_COL, &buffer[0], total_sz);
             else if (g_tw_synchronization_protocol == SEQUENTIAL && !g_st_disable_out)
                 fwrite(buffer, total_sz, 1, seq_model);
         }
