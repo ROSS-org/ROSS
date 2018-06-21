cb640e11e0bade2eab81f939c711e8bf34413414
Caitlin Ross <rossc3@rpi.edu>
2018-06-21 14:10:17 -0400

    separating core Damaris functionality from the ROSS repo

--- core/damaris/damaris.h
+++
@@ -1,8 +0,0 @@
-#include <ross.h>
-
-extern void st_set_damaris_parameters(int num_lp);
-extern void st_ross_damaris_init();
-extern void st_ross_damaris_finalize();
-extern void st_expose_data_damaris(tw_pe *me, tw_stime gvt, tw_statistics *s, int inst_type);
-extern void reset_block_counter();
-extern void damaris_error(const char *file, int line, int err, char *variable);
