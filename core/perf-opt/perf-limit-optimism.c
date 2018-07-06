d01aaa7a640932ad46554588df3f1a601151c0b9
Caitlin Ross <rossc3@rpi.edu>
2018-07-06 17:38:47 -0400

    completely changing the way the opt lookahead is changed

--- core/perf-opt/perf-limit-optimism.c
+++ core/perf-opt/perf-limit-optimism.c
@@ -9,6 +9,10 @@ static MPI_Op custom_opt_op;
 static int decrease = 1;
 static int rest_cycles = 0;
 static int max_rest = 10;
+static int beginning_wait = 200;
+static tw_stime *vt_diff_ema;
+static int first_iteration = 1;
+static double alpha = 0.25;
 
 static void test_method2(tw_pe *pe, double *global_data, int len);
 
@@ -30,13 +34,9 @@ void perf_init()
 {
     int i;
 
-    last_check.nevent_processed = tw_calloc(TW_LOC, "performance optimizations", sizeof(tw_stat), g_tw_nkp);
-    last_check.e_rbs = tw_calloc(TW_LOC, "performance optimizations", sizeof(tw_stat), g_tw_nkp);
+    vt_diff_ema = tw_calloc(TW_LOC, "performance optimizations", sizeof(tw_stime), g_tw_nkp);
     for (i = 0; i < g_tw_nkp; i++)
-    {
-        last_check.nevent_processed[i] = 0;
-        last_check.e_rbs[i] = 0;
-    }
+		vt_diff_ema[i] = 0;
 
 	MPI_Op_create((MPI_User_function *)perf_custom_reduction, 1, &custom_opt_op);
 }
@@ -57,11 +57,16 @@ static void reduce_kp_data_local(tw_pe *pe, double *results, int len)
         kp = tw_getkp(i);
 
         vt_diff = kp->last_time - pe->GVT;
+		if (first_iteration)
+			vt_diff_ema[i] = vt_diff;
+		else
+			vt_diff_ema[i] = alpha * vt_diff + (1.0 - alpha) * vt_diff_ema[i];
 
-		if (vt_diff > results[TIME_MAX])
-			results[TIME_MAX] = vt_diff;
+		if (vt_diff_ema[i] > results[TIME_MAX])
+			results[TIME_MAX] = vt_diff_ema[i];
     }
 
+	first_iteration = 0;
 }
 
 /* use Allreduce to exchange all of the relevant kp data to use for tuning
@@ -95,47 +100,64 @@ static void test_method2(tw_pe *pe, double *global_data, int len)
 {
 	int i, found = 0;
 	tw_stime min = DBL_MAX;
+	tw_stime max = -DBL_MAX;
 	tw_clock window_buffer;
 
-	// find the min
-	for (i = 0; i < len; i++)
+	if (!(beginning_wait-- > 0))
 	{
-		if (global_data[i] < min)
-			min = global_data[i];
-	}
-
-	if (!decrease && rest_cycles > max_rest)
-	{ 
-		// test to see if we should increase or decrease
-		window_buffer = g_tw_max_opt_lookahead * 0.1;
-
+		// find the min
 		for (i = 0; i < len; i++)
 		{
-			if (global_data[i] < pe->GVT + window_buffer)
+			if (global_data[i] < min  && global_data[i] > 0)
+				min = global_data[i];
+			if (global_data[i] > max  && global_data[i] > 0)
+				max = global_data[i];
+		}
+
+		printf("PE %ld, min = %f, max = %f\n", g_tw_mynode, min, max);
+		if (!decrease && rest_cycles > max_rest)
+		{ 
+			// test to see if we should increase or decrease
+			window_buffer = g_tw_max_opt_lookahead - (g_tw_max_opt_lookahead * 0.1);
+			printf("PE %ld: decrease = %d, window_buffer = %lu, g_tw_max_opt_lookahead = %llu \n", 
+					g_tw_mynode, decrease, window_buffer, g_tw_max_opt_lookahead);
+
+			for (i = 0; i < len; i++)
 			{
-				found = 1;
-				break;
+				if (global_data[i] < pe->GVT + window_buffer && global_data[i] > 0)
+				{
+					printf("PE %ld: found max less than window_buffer %lu lookahead: %llu\n", g_tw_mynode, window_buffer, g_tw_max_opt_lookahead);
+					found = 1;
+					break;
+				}
 			}
+
+			if (found) // now we need to decrease
+			{
+				decrease = 1;
+			}
+			else // we can increase
+			{
+				printf("PE %ld is increasing g_tw_max_opt_lookahead %llu\n", g_tw_mynode, g_tw_max_opt_lookahead);
+				g_tw_max_opt_lookahead += window_buffer;
+				printf("PE %ld increased g_tw_max_opt_lookahead %llu\n", g_tw_mynode, g_tw_max_opt_lookahead);
+			}
+			rest_cycles = 0;
 		}
 
-		if (found) // now we need to decrease
+		// first test if we need to decrease
+		if (decrease)
 		{
-			decrease = 1;
+			printf("PE %ld is about to decrease g_tw_max_opt_lookahead %llu\n", g_tw_mynode, g_tw_max_opt_lookahead);
+			g_tw_max_opt_lookahead = ceil(min);
+			decrease = 0;
+			printf("PE %ld decreased g_tw_max_opt_lookahead %llu\n", g_tw_mynode, g_tw_max_opt_lookahead);
 		}
-		else // we can increase
-		{
-			g_tw_max_opt_lookahead += window_buffer;
-		}
-		rest_cycles = 0;
+		rest_cycles++;
 	}
 
-	// first test if we need to decrease
-	if (decrease)
-	{
-		g_tw_max_opt_lookahead = min;
-		decrease = 0;
-	}
-	rest_cycles++;
+	if (g_tw_max_opt_lookahead < 10)
+		g_tw_max_opt_lookahead = 10;
 }
 
 //static void test_method1()
