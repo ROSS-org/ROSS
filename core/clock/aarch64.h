242416672e03f5b875f2d1f861736202620474c3
Marcel <marcel@brickporch.com>
2018-10-23 19:12:49 -0700

    remove barrier in aarch64 clock

--- core/clock/aarch64.h
+++ core/clock/aarch64.h
@@ -7,7 +7,7 @@ static inline tw_clock  tw_clock_read(void)
 {
         tw_clock result=0;
 #ifdef ROSS_timing
-       asm volatile ("isb;mrs %0, cntvct_el0" : "=r" (result));
+       asm volatile ("mrs %0, cntvct_el0" : "=r" (result));
 #endif
         return result;
 }
