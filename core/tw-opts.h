006f199cd4460f805516c5006b761ff3313cf197
Elsa Gonsiorowski (Uranus) <gonsiorowski1@llnl.gov>
2019-07-17 14:05:14 -0700

    added TWOPT_DOUBLE type

--- core/tw-opts.h
+++ core/tw-opts.h
@@ -8,6 +8,7 @@ enum tw_opttype
 	TWOPTTYPE_ULONGLONG,   /**< value must be an "unsigned long long*" */
 	TWOPTTYPE_UINT,        /**< value must be an "unsigned int*" */
 	TWOPTTYPE_STIME,       /**< value must be an "tw_stime*" */
+        TWOPTTYPE_DOUBLE,      /**< value must be a double * */
 	TWOPTTYPE_CHAR,        /**< value must be a char * */
         TWOPTTYPE_FLAG,        /**< value must be at "unsigned int*" */
 	TWOPTTYPE_SHOWHELP
@@ -28,6 +29,7 @@ struct tw_optdef
 #define TWOPT_ULONGLONG(n,v,h) { TWOPTTYPE_ULONGLONG, (n), (h), &(v) }
 #define TWOPT_UINT(n,v,h)  { TWOPTTYPE_UINT,  (n), (h), &(v) }
 #define TWOPT_STIME(n,v,h) { TWOPTTYPE_STIME, (n), (h), &(v) }
+#define TWOPT_DOUBLE(n,v,h)  { TWOPTTYPE_DOUBLE,(n), (h), &(v) }
 #define TWOPT_CHAR(n,v,h)  { TWOPTTYPE_CHAR,  (n), (h), &(v) }
 #define TWOPT_FLAG(n,v,h)  { TWOPTTYPE_FLAG,  (n), (h), &(v) }
 #define TWOPT_END()        { (tw_opttype)0,   NULL, NULL, NULL }
