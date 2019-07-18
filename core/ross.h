5813741bb8c2de3760c09d754e7f3e10cb1acf90
Elsa Gonsiorowski (Uranus) <gonsiorowski1@llnl.gov>
2019-07-17 17:11:06 -0700

    USE_TW_STIME_BITS configure option

--- core/ross.h
+++ core/ross.h
@@ -147,6 +147,9 @@ extern "C" {
 typedef unsigned long tw_peid;
 
 /* tw_stime -- Simulation time value for sim clock (NOT wall!) */
+#ifdef USE_TW_STIME_BITS
+#include "time/bits.h"
+#else
 typedef double tw_stime;
 #define MPI_TYPE_TW_STIME   MPI_DOUBLE
 #define TW_STIME_CRT(x)     (x)
@@ -154,6 +157,7 @@ typedef double tw_stime;
 #define TW_STIME_CMP(x, y)  (((x) < (y)) ? -1 : ((x) > (y)))
 #define TW_STIME_ADD(x, y)  ((x) + (y))
 #define TW_STIME_MAX        DBL_MAX
+#endif
 
 /* tw_lpid -- Logical Process "LP" id */
 //typedef unsigned long long tw_lpid;
